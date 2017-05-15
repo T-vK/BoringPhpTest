feed2d686c96e36c8d16baccd359eae38ced03a3
T-vK <t-v.k@gmx.net>
2017-05-15 08:21:31 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Aweseome Project</title>
+    <link href="libs/third-party/bootstrap-4/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+      body {
+        margin: 0;
+        padding: 0;
+        min-width: 700px;
+      }
+    </style>
+  </head>
+  <body>
+    
+    
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="libs/third-party/jquery-3.js"></script>
+    <script src="libs/third-party/tether-1.js"></script>
+    <script src="libs/third-party/bootstrap-4/js/bootstrap.min.js"></script>
+    <script src="libs/third-party/browser-polyfill.js"></script>
+    <script src="page-action-script.js"></script>
+  </body>
+</html>
