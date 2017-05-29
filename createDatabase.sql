cc49fee60601d17738faeed793b18295c925151e
pybi <leon.ramftler@gmail.com>
2017-05-29 18:23:58 +0200

    Updated the Database Script, added toggle Status functionality to the requestHandler, changed definition to Status in index

--- createDatabase.sql
+++ createDatabase.sql
@@ -1,9 +1,20 @@
 CREATE DATABASE toDoDB;
 USE toDoDB;
 
+
+CREATE TABLE toDoStatus (
+	ID int NOT NULL AUTO_INCREMENT,
+	Title varchar(255) NOT NULL,
+	PRIMARY KEY (ID)
+) ENGINE=INNODB;
+
+INSERT INTO toDoStatus (Title)
+VALUES ('open'), ('closed');
+
 CREATE TABLE toDoItems (
      ID int NOT NULL AUTO_INCREMENT,
-     Title varchar(255) NOT NULL,
-     ToDoStatus varchar(255),
-     PRIMARY KEY (ID)
-)AUTO_INCREMENT=0;
+     Title varchar(255),
+     ToDoStatus int NOT NULL,
+     PRIMARY KEY (ID),
+	 FOREIGN KEY (ToDoStatus) REFERENCES toDoStatus(ID)
+)ENGINE=INNODB;
